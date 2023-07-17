3f72fcb429730bf1d7093caa7f827f9bb0c7d008
woo200 <woo.jack.06@gmail.com>
2023-07-17 15:55:20 +0000

    Restructuring Imports

--- minedos/client/network/__init__.py
+++ minedos/client/network/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) John Woo. All rights reserved.
 # Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-from minedos.client.network.BasePacket import *
+from minedos.client.network.BasePacket import BasePacket
 from minedos.client.network.DataTypes import *
 from minedos.client.network.PacketTools import *
 
--- minedos/client/network/clientbound/__init__.py
+++ minedos/client/network/clientbound/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) John Woo. All rights reserved.
 # Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-from minedos.client.network.clientbound.EncryptionRequestPacket import *
+from minedos.client.network.clientbound.EncryptionRequestPacket import EncryptionRequestPacket
